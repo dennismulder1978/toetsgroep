2b399974df1ac4d2f4674286782f7b1c15da5fec
Dennis Werk <dnns.mldr@gmail.com>
2024-02-02 14:23:01 +0100

    4235

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 # # Toetsgroep AI
 # # Bloeddruk behandeling
 print()
+medicatie_lijst = ['hydrochloortiazide', 'enalapril', 'amlodipine']
 while True:
     
     # Input: 
-    medicatie_lijst = ['hydrochloortiazide', 'enalapril', 'amlodipine']
-    rr_syst = input('Wat is de systolische druk? ')
+    rr_syst = int(input('Wat is de systolische druk? '))
     print(f'Oude medicatie lijst: {medicatie_lijst}')
     print(f'Bovendruk: {rr_syst}')
-
-
-
 
 
     # Verwerking:
     def behandeling():
         pass
-
-
-
-
-
 
 
     # Output:
