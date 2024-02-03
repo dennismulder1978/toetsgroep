6a1b0752e53a32dfbc111694da7572bd825541dc
Dennis <dnns.mldr@gmail.com>
2024-02-03 07:17:56 +0100

    3456346

--- main.py
+++ main.py
@@ -4,18 +4,26 @@ print('----------------------------------------------------------------')
 medicatie_lijst = ['hydrochloortiazide', 'enalapril', 'amlodipine']
 
 while True:
-    # Input: 
+    # Gegevens: 
     rr_syst = int(input('Wat is de systolische druk? '))
-    print(f'Oude medicatie lijst: {medicatie_lijst}')
-    print(f'Bovendruk: {rr_syst}')
+    print(f'\tOude medicatie lijst: {medicatie_lijst}')
+    print(f'\tBovendruk: {rr_syst}')
 
 
     # Verwerking:
-    def behandeling():
-        pass
+    def behandeling(med_list, bovendruk):
+        if bovendruk > 140:
+            print('\tToevoegen van metoprolol.')
+            med_list.append('metoprolol')
+        elif bovendruk < 105:
+            print('\tVerwijderen van laatste medicatie.')
+            med_list.pop()
+        else:
+            print('\tGeen aanpassing nodig.')
+        return med_list
 
 
-    # Output:
-    medicatie_lijst = behandeling()
-    print(f'Nieuwe medicatie lijst: {medicatie_lijst}')
+    # Uitkomst:
+    medicatie_lijst = behandeling(med_list=medicatie_lijst, bovendruk=rr_syst)
+    print(f'\tNieuwe medicatie lijst: {medicatie_lijst}')
     print('----------------------------------------------------------------', end="\n\n")
